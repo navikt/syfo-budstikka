81db2c97fdcaffc51f5f52e44e4630e235986e99
André Wittwer <andre.wittwer@nav.no>
2026-07-07 15:34:08 +0200

    fix: logger name

--- src/main/kotlin/no/nav/budstikka/infrastructure/kafka/formidling/InboxHandler.kt
+++ src/main/kotlin/no/nav/budstikka/infrastructure/kafka/formidling/InboxHandler.kt
@@ -4,10 +4,9 @@ import kotlinx.serialization.json.Json
 import no.nav.budstikka.infrastructure.kafka.config.MessageHandler
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
-import java.lang.invoke.MethodHandles
 
 class InboxHandler : MessageHandler<String, String?> {
-    private val logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass())
+    private val logger = LoggerFactory.getLogger(InboxHandler::class.java)
     private val json = Json { ignoreUnknownKeys = true }
 
     override suspend fun handle(record: ConsumerRecord<String, String?>) {
