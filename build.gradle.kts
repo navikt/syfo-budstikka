cd3d5d30d709292d5fd72243df405483b0285ced
André Wittwer <andre.wittwer@nav.no>
2026-07-07 07:08:59 +0200

    feat: add libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,15 @@
+buildscript {
+    dependencies {
+        classpath(libs.flyway.database.postgresql)
+    }
+}
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
-    alias(ktorLibs.plugins.ktor)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ktor)
     alias(libs.plugins.ktlint)
+    alias(libs.plugins.flyway)
 }
 
 group = "no.nav.syfo"
@@ -12,14 +20,57 @@ application {
 }
 
 kotlin {
-    jvmToolchain(25)
+    jvmToolchain(
+        libs.versions.java
+            .get()
+            .toInt(),
+    )
 }
-dependencies {
-    implementation(ktorLibs.server.config.yaml)
-    implementation(ktorLibs.server.core)
-    implementation(ktorLibs.server.netty)
-    implementation(libs.logback.classic)
 
-    testImplementation(kotlin("test"))
-    testImplementation(ktorLibs.server.testHost)
+dependencies {
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.call.id)
+    implementation(libs.ktor.server.call.logging)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.server.di)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.hikari)
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.exposed.kotlin.datetime)
+    implementation(libs.flyway.core)
+    implementation(libs.flyway.database.postgresql)
+    implementation(libs.postgresql)
+    implementation(libs.logback.classic)
+    implementation(libs.logstash.logback.encoder)
+    implementation(libs.micrometer.registry.prometheus)
+}
+
+tasks {
+    register("printVersion") {
+        description = "Print the version of the app"
+        doLast {
+            println(project.version)
+        }
+    }
+
+    named("check") {
+        dependsOn("ktlintCheck")
+    }
+
+    shadowJar {
+        filesMatching("META-INF/services/**") {
+            duplicatesStrategy = DuplicatesStrategy.WARN
+        }
+        mergeServiceFiles()
+        archiveFileName.set("app.jar")
+        archiveClassifier.set("")
+        archiveVersion.set("")
+    }
 }
