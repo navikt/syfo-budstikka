b21678cc2fdb098a8e25b6f1aebf2cbe601a36f9
André Wittwer <andre.wittwer@nav.no>
2026-07-27 14:38:32 +0200

    fix: mdc logging (#142)
    * fix: mdc key logging
    
    * fix: tests

--- src/main/kotlin/no/nav/budstikka/application/MdcKeys.kt
+++ src/main/kotlin/no/nav/budstikka/application/MdcKeys.kt
@@ -1,9 +1,12 @@
 package no.nav.budstikka.application
 
 object MdcKeys {
-    const val DELIVERY_CHANNEL = "delivery_channel"
-    const val EVENT_ID = "event_id"
-    const val REFERENCE = "reference"
     const val CONSUMER = "consumer"
+    const val DELIVERY_CHANNEL = "delivery_channel"
+    const val DELIVERY_ID = "delivery_id"
+    const val EVENT_ID = "event_id"
+    const val HANDLER = "handler"
+    const val REASON = "reason"
+    const val REFERENCE = "reference"
     const val WORKER = "worker"
 }
